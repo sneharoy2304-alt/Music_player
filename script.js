5754fe2fb24474ad53c3ad4d2825f8e2fdaa3b1f
Sneha Kumari <sneharoy2304@gmail.com>
2025-09-22 18:21:00 +0530

    fc

---
+++ script.js
@@ -0,0 +1,134 @@
+const musicContainer = document.getElementById('audio-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+// Song titles
+const songs = ['Headlines', 'Drake - Slime You Out ft. SZA', 'Shot For Me', 'The Town', 'The Weeknd-After Hours']; //Add the songs name here first
+
+// Keep track of song
+let songIndex = 4;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+
+  console.log(`Loading song: ${song}`);
+  console.log(`Audio source: ${audio.src}`);
+  console.log(`Cover image source: ${cover.src}`);
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
+// Previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+//FUNTION FOR PAUSING/PLAYING SONG WHILE CLICKING ON SPACEBAR ON KEYBOARD, NEXT SONG BY CLICKING ON RIGHT ARROW AND PREVIOUS SONG BY CLICKING ON LEFT ARROW
+// Function to handle keyboard events
+function handleKeyEvents(e) {
+  if (e.code === 'Space') {
+    const isPlaying = musicContainer.classList.contains('play');
+    if (isPlaying) {
+      pauseSong();
+    } else {
+      playSong();
+    }
+  } else if (e.code === 'ArrowRight') {
+    nextSong();
+  } else if (e.code === 'ArrowLeft') {
+    prevSong();
+  }
+}
+
+// Event listener for keyboard events
+document.addEventListener('keydown', handleKeyEvents);
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth
+  const clickX = e.offsetX
+  const duration = audio.duration
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
